4611ff97710fe7d6e1f67b7e7d713ea8f84fe399
+ <+>
2022-11-22 00:42:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()+1);
+
+let num3 = 37;
+let num4 = 11;
+let mod = Math.ceil(num3 % num4);
+
+let max = Math.max(15,15,17,20);
